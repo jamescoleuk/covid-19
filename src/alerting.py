abd85caa7c67e32e6471cc14b72177b2763f17f4
James Cole <jamescole.uk@gmail.com>
2020-03-10 16:03:40 +0000

    Improve logging

--- src/alerting.py
+++ src/alerting.py
@@ -3,7 +3,8 @@ from twilio.rest import Client
 def send(changed, account_sid, auth_token, from_num, to_num):
     client = Client(account_sid, auth_token)
     for change in changed:
-            message = client.messages.create(
-                to=to_num, 
-                from_=from_num,
-                body=change)
+        print(f"Sending a message to {to_num}: {change}")
+        client.messages.create(
+            to=to_num, 
+            from_=from_num,
+            body=change)
